b6ecc9626709abf9f4d3e79494209e8892f3b983
Anderson Domingos <anderson@mica12.com.br>
2026-09-17 18:50:37 +0000

    refactor: corrige semantica HTTP da API de funcionarios

--- WebApi_Funcionarios/Controllers/FuncionarioController.cs
+++ WebApi_Funcionarios/Controllers/FuncionarioController.cs
@@ -1,60 +1,83 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebApi_ASPNETCore.Models;
 using WebApi_ASPNETCore.Service.FuncionarioService;
 
-namespace WebApi_ASPNETCore.Controllers
+namespace WebApi_ASPNETCore.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class FuncionarioController : ControllerBase
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class FuncionarioController : ControllerBase
+    private readonly IFuncionarioInterface _funcionarioService;
+
+    public FuncionarioController(IFuncionarioInterface funcionarioService)
     {
-        private readonly IFuncionarioInterface _funcionarioInterface;
+        _funcionarioService = funcionarioService;
+    }
 
-        public FuncionarioController(IFuncionarioInterface funcionarioInterface)
-        {
-            _funcionarioInterface = funcionarioInterface;
-        }
+    [HttpGet]
+    public async Task<ActionResult<ServiceResponse<List<FuncionarioModel>>>> GetFuncionarios()
+    {
+        var response = await _funcionarioService.GetFuncionarios();
+        return Ok(response);
+    }
 
-        [HttpGet]
-        public async Task<ActionResult<ServiceResponse<List<FuncionarioModel>>>> GetFuncionarios()
-        {
-             return Ok( await _funcionarioInterface.GetFuncionarios() );
-        }
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<ServiceResponse<FuncionarioModel>>> GetFuncionarioById(int id)
+    {
+        var response = await _funcionarioService.GetFuncionariosById(id);
 
-        [HttpGet("{id}")]
-        public async Task<ActionResult<ServiceResponse<FuncionarioModel>>> GetFuncionariosById(int id)
-        {
-            return Ok(await _funcionarioInterface.GetFuncionariosById(id));
-        }
+        if (!response.Sucesso)
+            return NotFound(response);
 
-        [HttpPost]
-        public async Task<ActionResult<ServiceResponse<List<FuncionarioModel>>>> CreateFuncionarios(FuncionarioModel modelCreate)
-        {
-            return Ok(await _funcionarioInterface.CreateFuncionarios(modelCreate));
-        }
+        return Ok(response);
+    }
 
-        [HttpPut]
-        public async Task<ActionResult<ServiceResponse<List<FuncionarioModel>>>> UpdateFuncionarios(FuncionarioModel modelCreate, int id)
-        {
-            return Ok(await _funcionarioInterface.UpdateFuncionarios(modelCreate, id));
-        }
+    [HttpPost]
+    public async Task<ActionResult<ServiceResponse<List<FuncionarioModel>>>> CreateFuncionario(
+        FuncionarioModel model)
+    {
+        var response = await _funcionarioService.CreateFuncionarios(model);
 
-        [HttpPut("inativaFuncionario")]
-        public async Task<ActionResult<ServiceResponse<List<FuncionarioModel>>>> InativaFuncionario(int id)
-        {
-            return Ok(await _funcionarioInterface.InativaFuncionario(id));
-        }
+        return CreatedAtAction(
+            nameof(GetFuncionarioById),
+            new { id = model.Id },
+            response);
+    }
 
-        [HttpDelete]
-        public async Task<ActionResult<ServiceResponse<List<FuncionarioModel>>>> DeleteFuncionarios(int id)
-        {
-            return Ok(await _funcionarioInterface.DeleteFuncionarios(id));
-        }
+    [HttpPut("{id:int}")]
+    public async Task<ActionResult<ServiceResponse<List<FuncionarioModel>>>> UpdateFuncionario(
+        int id,
+        FuncionarioModel model)
+    {
+        var response = await _funcionarioService.UpdateFuncionarios(model, id);
 
-      
+        if (!response.Sucesso)
+            return NotFound(response);
+
+        return Ok(response);
+    }
+
+    [HttpPatch("{id:int}/deactivate")]
+    public async Task<ActionResult<ServiceResponse<List<FuncionarioModel>>>> DeactivateFuncionario(
+        int id)
+    {
+        var response = await _funcionarioService.InativaFuncionario(id);
+
+        if (!response.Sucesso)
+            return NotFound(response);
+
+        return Ok(response);
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<ActionResult<ServiceResponse<List<FuncionarioModel>>>> DeleteFuncionario(int id)
+    {
+        var response = await _funcionarioService.DeleteFuncionarios(id);
+
+        if (!response.Sucesso)
+            return NotFound(response);
+
+        return Ok(response);
     }
 }
